5e556e7eff0a82ba56728b5d9e4915c51b4b6e71
Kemal Durukan MERT <103702848+KemalDurukanMERT@users.noreply.github.com>
2024-09-20 20:28:51 +0200

    functional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
+
+  let name = "Cosmios"
+
+  let pStyle = {
+    color: "aqua"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+    <div style={{
+      // color: "red",
+    }}>
+
+      <Header />
+
+      Merhaba {name}
+      <p  style={pStyle}>Bu bir internal css ornegidir</p>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+
+        <h1>Bu bir Functional Component</h1>
+
+    )
+}
+
+
+export default Header
